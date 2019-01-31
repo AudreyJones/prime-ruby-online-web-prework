6e5cafc5e173b90d23b09095cd94b8640b769049
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-31 14:53:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,16 @@
-def prime?(number)
-
+def prime?(n)
 #How can I represent that: any number that is only divisible by one (and itself), is prime....everything else is not prime?
-  if (number % number) && (number % 1)  == 0
-    puts "this number IS a prime number"
-    true
-  else
-    puts "this is NOT a prime number"
-    false
+  if n <= 3
+    return n > 1
+  else (n % 2 == 0) or (n % 3 == 0)
+    return false
   end
+  i = 5
+  while i * i <= n
+    if (n % i == 0) or (n % (i + 2) == 0)
+      return false
+    i = i + 6
+  end
+  return true
+  
 end
