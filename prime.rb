b90f99750145f843f5e4f1489cfe73679e01bf94
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-31 13:52:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(number)
-  d = (2..9)
+  d = (2..11)
 #How can I represent that: any number that is only divisible by one (and itself), is prime....everything else is not prime?
   if number % d == 0
     puts "this number IS a prime number"
