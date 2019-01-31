e55072284a7f586f989c0d30931966a8bb6ad2f3
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-31 15:15:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(n)
 #How can I represent that: any number that is only divisible by one (and itself), is prime....everything else is not prime?
   if n <= 3
     return false
-  else (n % 2 == 0) or (n % 3 == 0)
+  elsif (n % 2 == 0) or (n % 3 == 0)
     return false
   end
   i = 5
